54de656797ec72866fd4c66ffa4a0eebe1c3c7de
streetwraith <streetwraith@fake.email>
2018-09-05 15:12:25 +0200

    added some early data analysis experiments

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Configuration = require('./config.js');
 const Services = require('./services.js');
 const moment = require('moment');
 
+require('console.table');
+const _ = require('lodash');
+
 // open mongo connection
 Persistence.mongoose.connect(Configuration.db.url, { useNewUrlParser: true });
 Persistence.mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -27,43 +30,108 @@ process.on('SIGINT', shutdown);
 // process.on('uncaughtException', shutdown);
 // process.on('unhandledRejection', shutdown);
 
-const getTournaments = async function(dateStart, dateEnd, format) {
-	let tournaments = await Services.getTournaments(dateStart, dateEnd, format)
-	.then(tournaments => tournaments.filter(tournament => tournament['format'] === 'modern' || tournament['format'] === 'mixed'))
-	.then(tournaments => tournaments.filter(tournament => Configuration.tournamentTypes.some(tournamentType => tournament.name.match(tournamentType.regex))))
-	.then(tournaments => 
-		Promise.all(tournaments.map(Persistence.persistTournament))
-	);
+const findByArchetype = async function(archetype) {
+	let matches = [];
+	await Persistence.model.Deck.find({})
+	.then(decks => {
+		decks.forEach(deck => {
+			let mainboardCards = deck.mainboard.map(d => d._id);
+			let matching = _.difference(archetype.includedCards, mainboardCards).length === 0
+				&& _.difference(mainboardCards, archetype.excludedCards).length === mainboardCards.length;
+			if(matching) {
+				matches.push(deck);
+			}
+		})
+	});
+
+	return matches;
 }
 
-const getDecks = async function(tournamentId, dateStart) {
-	let query = {};
-	if(tournamentId) {
-		query._id = tournamentId
-	} else if(dateStart) {
-		query.date = {
-			$gte: moment(dateStart, 'YYYY-MM-DD')
-		}
+const printStats = function(decks, board) {
+	console.log(board);
+	let totalDecks = decks.length;
+	let cardCounts = {};
+
+	decks.forEach(deck => {
+		deck[board].forEach(card => {
+			let count = cardCounts[card._id] || 0;
+			cardCounts[card._id] = count+card.quantity;
+		});
+	});
+
+	let result = [];
+	for (var cardName in cardCounts) {
+		let decksCount = decks.filter(deck => {return _.some(deck[board], {'_id': cardName}); }).length;
+		result.push({
+			name: cardName,
+			total: cardCounts[cardName],
+			decks: decksCount,
+			average: Math.round(cardCounts[cardName]/decksCount * 1000) / 1000,
+			decksFormatted: decksCount+'/'+totalDecks+' ('+parseInt(decksCount/totalDecks*100)+'%)'
+		});
 	}
 
-	let tournaments = await Persistence.model.Tournament.find(query)
-	.then(async function(tournaments) {
-			let allDecks = [];
-			for(var i = 0; i < tournaments.length; i++) {
-				let tournament = tournaments[i];
-				let decks = await Services.getDecksForTournament(tournament._id);
-				await Promise.all(decks.map(Persistence.persistDeck));
-				allDecks.concat(decks);
-			}
-			return allDecks;
-		}
+	result = _.orderBy(result, ['decks'], ['desc'])
+	result = _.map(result, function(r) { return _.omit(r, ['decks']); });
+	console.table(
+		result
 	);
 }
 
 const run = async function() {
 
-	// await getTournaments('2018-08-01', '2018-08-31', 'modern');
-	await getDecks(null, '2018-08-01');
+	let decks = await findByArchetype({
+		name: 'Blue Moon',
+		format: 'modern',
+		includedCards: ['Snapcaster Mage', 'Blood Moon', 'Lightning Bolt', 'Scalding Tarn'],
+		excludedCards: ['Tarmogoyf'],
+	});
+	console.log('blue moon');
+	printStats(decks, 'mainboard');
+	printStats(decks, 'sideboard');
+
+	decks = await findByArchetype({
+		name: 'Fetchless Storm',
+		format: 'modern',
+		includedCards: ['Shivan Reef', 'Past in Flames', 'Grapeshot'],
+		excludedCards: ['Scalding Tarn'],
+	});
+	console.log('fetchless storm');
+	printStats(decks, 'mainboard');
+	printStats(decks, 'sideboard');
+
+	decks = await findByArchetype({
+		name: 'GDS',
+		format: 'modern',
+		includedCards: ['Death\'s Shadow', 'Street Wraith', 'Gurmag Angler'],
+		excludedCards: ['Tarmogoyf'],
+	});
+	console.log('gds');
+	printStats(decks, 'mainboard');
+	printStats(decks, 'sideboard');
+
+	decks = await findByArchetype({
+		name: 'Tron',
+		format: 'modern',
+		includedCards: ['Urza\'s Tower', 'Urza\'s Power Plant',  'Urza\'s Mine', 'Sylvan Scrying'],
+		excludedCards: ['Eldrazi Temple'],
+	});
+	console.log('tron');
+	printStats(decks, 'mainboard');
+	printStats(decks, 'sideboard');
+
+	// printStats(decks, 'mainboard');
+	// printStats(decks, 'sideboard');
+
+	// decks = await findByArchetype({
+	// 	name: 'All',
+	// 	format: 'modern',
+	// 	includedCards: [],
+	// 	excludedCards: [],
+	// });
+	// console.log('all');
+	// printStats(decks, 'mainboard');
+	// printStats(decks, 'sideboard');
 
 	return;
 };
